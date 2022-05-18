2a103258b36a18f8b7c2a038625caf1c6c87cdc2
phammanhcuong1996 <you@example.com>
2022-05-19 00:57:55 +0700

    Edit-detail

--- resources/views/fe/index/elements/product_list.blade.php
+++ resources/views/fe/index/elements/product_list.blade.php
@@ -2,7 +2,7 @@
     @foreach ($productByCategory as $product)
         <div class="col-12 col-sm-6 col-md-3 bg-ranger">
             <div class="card" >
-                <a href="{{ route('fe.products.show', $product->id) }}"><img style="width: 250px;height: 250px" src="{{ $product->image}}" class="card-img-top">
+                <a href="{{ route('fe.products.detail', $product->id) }}"><img style="width: 250px;height: 250px" src="{{ $product->image}}" class="card-img-top">
                 </a>
                 <div class="card-body" style="text-align: center">
                     <h2 href="" class="card-content" >{{ $product->name }}</h2>
