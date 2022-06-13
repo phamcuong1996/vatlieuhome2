93000d94af2d8c71d127a01eee074b4047553507
phammanhcuong1996 <you@example.com>
2022-06-13 17:34:02 +0700

    123

--- resources/views/products/carts.blade.php
+++ resources/views/products/carts.blade.php
@@ -5,14 +5,55 @@
             @include('products.components.cart_component')
         </div>
     @else
-        <h4 style="text-align: center">Chưa có sản phẩm nào được thêm vào giỏ hàng</h4>
+        <h2>Chưa có sản phẩm</h2>
     @endif
-    <script >
-        $(document).ready(function() {
-            $(".cart_update").click(function () {
-                alert(123);
-            });
-        })
+    <script>
+        function cartUpdate(event) {
+            event.preventDefault();
+            let urlUpdateCart = $('.update_cart_url').data('url');
+            let product_id = $(this).data('product_id');
+            let quantity = $(this).parents('tr').find('input.quantity').val();
 
+            $.ajax({
+                type: "GET",
+                url: urlUpdateCart,
+                data: {product_id: product_id, quantity: quantity},
+                success: function (data) {
+                    if (data.code === 200) {
+                        $('.cart_wapper').html(data.cart_component);
+                        alert('Cập Nhật Thành Công');
+                    }
+                },
+                error: function () {
+
+                }
+            })
+        }
+
+        function cartDelete(event) {
+            event.preventDefault();
+            let urlDeleteCart = $('.carts').data('url');
+            let product_id = $(this).data('product_id');
+            $.ajax({
+                type: "GET",
+                url: urlDeleteCart,
+                data: {product_id: product_id},
+                success: function (data) {
+                    if (data.code === 200) {
+                        $('.cart_wapper').html(data.cart_component)
+                        alert('Xóa Sản Phẩm Thành Công');
+                    }
+                },
+                error: function () {
+
+                }
+            })
+        }
+
+        $(function () {
+            $(document).on('click', '.cart_update', cartUpdate);
+            $(document).on('click', '.cart_delete', cartDelete);
+        });
     </script>
 @endsection
+
