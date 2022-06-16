5d80bf03201ca86d3f9be78322b44c1db9cd3cd2
phammanhcuong1996 <you@example.com>
2022-06-16 22:10:37 +0700

    edit buttom home

--- resources/views/fe/layout/home.blade.php
+++ resources/views/fe/layout/home.blade.php
@@ -97,9 +97,9 @@
                             <div class="nav-panel__nav-links nav-links">
                                 <ul class="nav-links__list">
                                     <li class="nav-links__item ">
-                                        <a class="nav-links__item-link" href="https://themeforest.net/item/stroyka-tools-store-html-template/23326943">
+                                        <a class="nav-links__item-link" href="{{route('index')}}">
                                             <div class="nav-links__item-body">
-                                                Giới Thiệu
+                                                Trang Chủ
                                             </div>
                                         </a>
                                     </li>
